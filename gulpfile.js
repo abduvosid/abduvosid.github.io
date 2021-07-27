c94bbc43ff62dda4e0839afc86f3e0039897ecaa
Abuvosid <vosid071120@gmail.com>
2021-07-27 12:44:11 +0500

    first commmit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename = require("gulp-rename");
+const browserslist = require('browserslist');
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
+function process (source, opts) {
+    const browsers = browserslist(opts.overrideBrowserslist, {
+      stats: opts.stats,
+      path:  opts.file,
+      env:   opts.env
+    });
+    browserslist({
+        server: {
+            baseDir: "src"
+        }
+    })
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }));
+}
